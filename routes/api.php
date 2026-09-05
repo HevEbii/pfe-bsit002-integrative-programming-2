a35d7294b308a9ae08444ee9fc1bafbb2b03ad87
Eddiel Billy Celi <eddielbillyc10@gmail.com>
2026-09-05 16:52:02 +0800

    Implement CRUD API for employees

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EmployeeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -21,3 +22,9 @@ Route::get('/students', function () {
         ]
     ]);
 });
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employees', [EmployeeController::class, 'store']);
+Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
